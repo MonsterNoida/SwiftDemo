345057961633ada601dcdad160cd2b844fc60e38
Himanshu Ahuja <ahujahimanshu@Himanshus-Mac-Pro.local>
2017-03-22 13:47:58 +0530

    Add table view

--- swift-1/ProductsViewController.swift
+++ swift-1/ProductsViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+class ProductsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var fullNameLabel:UILabel!
+    @IBOutlet weak var tableview:UITableView!
+    public var prodArr :Array<Any> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,4 +25,18 @@ class ProductsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prodArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let productModel = prodArr[indexPath.row] as! ProductModel
+        
+        cell.textLabel?.text = productModel.product! + " And cost is " + productModel.cost!
+
+        return cell
+    }
 }
